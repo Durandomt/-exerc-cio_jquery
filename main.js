0c1f100942d7844ce73fe68add21f96eb3dbb8da
Durand <candeira.durand34@gmail.com>
2023-04-13 23:47:53 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+
+    $('form').on('submit', function(e){
+        e.preventDefault();
+        
+
+        const checkList = $('#check-list').val();
+        const novoItem = $('<li style = "list-style:circle";) ></li>');
+        
+        
+        $('ul').click(function(){
+            $('li').css('text-decoration', 'line-through')
+        })
+        
+        
+        
+        
+        $(`<li>${checkList}</li>`).appendTo(novoItem);
+        $(novoItem).appendTo('ul');
+        $('#check-list').val('');
+
+        
+        
+    })
+})

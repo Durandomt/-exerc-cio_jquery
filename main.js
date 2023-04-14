fd7818f9674172f365f7aab58f6c56d420860282
Durand <candeira.durand34@gmail.com>
2023-04-13 23:54:23 -0300

    fix: ementação de código

--- main.js
+++ main.js
@@ -3,23 +3,16 @@ $(document).ready(function(){
     $('form').on('submit', function(e){
         e.preventDefault();
         
-
         const checkList = $('#check-list').val();
         const novoItem = $('<li style = "list-style:circle";) ></li>');
-        
         
         $('ul').click(function(){
             $('li').css('text-decoration', 'line-through')
         })
         
-        
-        
-        
         $(`<li>${checkList}</li>`).appendTo(novoItem);
         $(novoItem).appendTo('ul');
         $('#check-list').val('');
 
-        
-        
-    })
+        })
 })
